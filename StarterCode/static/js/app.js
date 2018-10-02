703a593bdf72c9ca669685a187d721dbf7e9c57d
Rori Macaulay Cooper <rori.m.cooper@gmail.com>
2018-10-01 20:35:45 -0500

    fix tbl display

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,26 +1,51 @@
 // from data.js
 var tableData = data;
+
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+
 // Console.log the ufo data from data.js
+var dataFilter = 0;
+ufos = data
+
 console.log(data);
 
+data.forEach((ufoSighting) => {
+	var row = tbody.append("tr");
+	Object.entries(ufoSighting).forEach(([key, value]) => {
+		var cell = tbody.append("td");
+		cell.text(value);
+	});
+});
+
+// Enable filter button
 var submit = d3.select("#filter-btn");
+
 submit.on("click", function () {
-	// Prevent the page from refreshing
-    d3.event.preventDefault();
-    d3.select('tbody').selectAll('tr').remove();
-	// Select the input element and get the raw HTML node
+
+	// Disable page from refreshing
+	d3.event.preventDefault();
+
+	// Select datetime as input
 	var inputElement = d3.select("#datetime");
-	// Get the value property of the input element
+
+	// Obtain search value
 	var inputValue = inputElement.property("value");
-	var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-	// console.log(filteredData);
-	filteredData.forEach((ufoSighting) => {
+
+	var filteredData = ufos.filter(ufo => ufo.datetime === inputValue);
+	buildTable(filteredData);
+
+});
+
+// Perform datetime filter on table data
+function buildTable(dataFilter) {
+
+	tbody.html("");
+	dataFilter.forEach((ufoSighting) => {
 		var row = tbody.append("tr");
 		Object.entries(ufoSighting).forEach(([key, value]) => {
-			var cell = row.append("td");
+			var cell = tbody.append("td");
 			cell.text(value);
 		});
 	});
-});
+}
